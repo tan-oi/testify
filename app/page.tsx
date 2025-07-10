741efa5a5f6134af4831ed73494c388616253852
Tanay <tanayghoriwala001@gmail.com>
2025-07-10 13:19:26 +0530

    auth redirect

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await auth();
   console.log(session);
-
+  if(!session) redirect("/auth");
   if (session?.user) redirect("/dashboard");
   return (
-    <div>
+    <div className="">
      
     </div>
   );
